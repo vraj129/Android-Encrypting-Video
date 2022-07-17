b8c38b4d3ae2e7583e116e3e0383ec24dbb2bf45
vraj129 <vrajpatelv@protonmail.com>
2022-07-17 11:55:11 +0530

    Changes

--- app/src/main/java/com/example/offlinefeature/MainActivity.java
+++ app/src/main/java/com/example/offlinefeature/MainActivity.java
@@ -15,7 +15,13 @@ import android.webkit.CookieManager;
 import android.webkit.URLUtil;
 import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.DownloadListener;
 
 import java.io.File;
 import java.io.IOException;
@@ -28,54 +34,60 @@ import javax.crypto.NoSuchPaddingException;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn,btn1,btn2;
-    boolean isDownloaded = false;
+    private Button btn,btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AndroidNetworking.initialize(getApplicationContext());
+        String title="video.mp4";
         String url = "https://i.imgur.com/7bMqysJ.mp4";
         DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
         btn = findViewById(R.id.button);
-        btn1 = findViewById(R.id.button1);
         btn2 = findViewById(R.id.button2);
         VideoView videoView = findViewById(R.id.videoview);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                    DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-                    String title = URLUtil.guessFileName(url,null,null);
-                    request.setTitle(title);
-                    String cookie = CookieManager.getInstance().getCookie(url);
-                    request.addRequestHeader("cookie",cookie);
-                    request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,title);
-                    downloadManager.enqueue(request);
-            }
-        });
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String title = URLUtil.guessFileName(url,null,null);
                 File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-                File plain = new File(path,title);
-
-                File eplain = new File(path,title+"encrypted.txt");
-                try {
-                    DES.encryptDecrypt("12345678", Cipher.ENCRYPT_MODE,plain,eplain);
-                    plain.delete();
-                } catch (InvalidKeyException | NoSuchAlgorithmException | InvalidKeySpecException | NoSuchPaddingException | IOException e) {
-                    e.printStackTrace();
+                if(path.exists()){
+                    path.delete();
+                    AndroidNetworking.forceCancelAll();
                 }
+                AndroidNetworking.download(url,""+ path,title)
+                        .setTag("downloadTest")
+                        .setPriority(Priority.MEDIUM)
+                        .build()
+                        .startDownload(new DownloadListener() {
+                            @Override
+                            public void onDownloadComplete() {
+                                Toast.makeText(MainActivity.this,"Downloaded",Toast.LENGTH_SHORT).show();
+                               // String title = URLUtil.guessFileName(url,null,null);
+                                File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                                File plain = new File(path,title);
+
+                                File eplain = new File(path,title+"encrypted.txt");
+                                try {
+                                    DES.encryptDecrypt("12345678", Cipher.ENCRYPT_MODE,plain,eplain);
+                                    plain.delete();
+                                } catch (InvalidKeyException | NoSuchAlgorithmException | InvalidKeySpecException | NoSuchPaddingException | IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            @Override
+                            public void onError(ANError anError) {
+                                Log.e("ERROR",anError.toString());
+                            }
+                        });
             }
         });
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String title = URLUtil.guessFileName(url,null,null);
                 File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
                 File eplain = new File(path,title+"encrypted.txt");
-                //File dec = new File(path,title+"dec.mp4");
 
                 try {
                     File tempVideo = File.createTempFile("test","mp4");
